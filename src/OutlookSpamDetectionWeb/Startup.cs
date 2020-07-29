10d32a4016b71bf334ecfc09e7772974441b03e7
juan.calderon <jc.calderon@outlook.com>
2020-07-28 23:01:47 -0400

    Added Swagger for API doc

--- src/OutlookSpamDetectionWeb/Startup.cs
+++ src/OutlookSpamDetectionWeb/Startup.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace OutlookSpamDetectionWeb
 {
@@ -19,11 +22,13 @@ namespace OutlookSpamDetectionWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            this.ConfigureSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -33,10 +38,38 @@ namespace OutlookSpamDetectionWeb
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Outlook Spam Detection API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Outlook Spam Detection API",
+                    Description = "Predict whether an email is spam",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Juan Carlos Calderón Cuellar",
+                        Email = "jc.calderon@outlook.com",
+                        Url = new Uri("https://github.com/jc-calderon"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://github.com/jc-calderon/outlook-spam-detection/blob/master/LICENSE"),
+                    }
+                });
             });
         }
     }
